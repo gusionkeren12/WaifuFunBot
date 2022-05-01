07904367c0c8ac2b6dab9da974100f223f7de455
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-01 19:48:40 +0530

    Update __init__.py

--- nksama/__init__.py
+++ nksama/__init__.py
@@ -10,19 +10,4 @@ bot = Client(
     plugins=dict(root=f"{__name__}/plugins")
 )
 
-r = os.environ.get("REDIS_URL").split(":")
-REDIS_PASSWORD = r[2]
-REDIS_PORT = r[1]
-
-REDIS_DB = Redis(
-    host=r[0],
-    password=REDIS_PASSWORD,
-    port=REDIS_PORT,
-    decode_responses=True,
-)
-
-REDIS_DB.ping()
-
-
-
 help_message = []
