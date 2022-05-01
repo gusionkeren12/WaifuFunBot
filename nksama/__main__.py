a7ef98a29874c0392bce92238c6fd0840b66f138
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-01 19:56:49 +0530

    Update __main__.py

--- nksama/__main__.py
+++ nksama/__main__.py
@@ -4,6 +4,5 @@ from nksama import bot , musicbot
 
 if __name__ == "__main__":
     bot.run()
-    musicbot.start()
-    bot.send_message(-1001544622735 , "Hello there I'm Now online")
+    bot.send_message(-1001590378481 , "we made new bot and new bot still run")
     
