05047e205292e19111f56f5840b36e84024e7bcd
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-10 01:25:42 +0530

    Update waifu.py

--- nksama/plugins/waifu.py
+++ nksama/plugins/waifu.py
@@ -8,5 +8,5 @@ from pyrogram import filters
 @bot.on_message(filters.command('waifu'))
 def waifu(_,message):
     x = requests.get("https://api.waifu.pics/sfw/waifu").json()
-    await bot.message_photo(message.chat_id,photo=x)    
+    await bot.message_photo(message.chat.id,photo=x)    
      
