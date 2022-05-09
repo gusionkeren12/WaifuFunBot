943e0048075093676d6fe5dc811a2300e9724f0e
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-09 15:52:02 +0530

    Update __main__.py

--- nksama/__main__.py
+++ nksama/__main__.py
@@ -4,5 +4,6 @@ from nksama import bot
 
 if __name__ == "__main__":
     bot.run()
-    bot.send_message("@unitedsupport" , "YuukiRobot Alive!")
+    
+    bot.send_photo("@unitedsupport" , photo="https://telegra.ph/file/0f4b6a40dc4eb52587234.jpg","YuukiRobot Alive!")
     
