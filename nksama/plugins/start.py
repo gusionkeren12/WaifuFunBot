ca8739cea671b7e49390f3b7b0ab9bef720540e1
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-09 16:04:24 +0530

    Update start.py

--- nksama/plugins/start.py
+++ nksama/plugins/start.py
@@ -8,6 +8,6 @@ from nksama import help_message
 @bot.on_message(filters.command('start'))
 def start(_,message):
 
-    bot.send_message(message.chat.id , "Hello there i'm Yuuki-San\nI'll help you to manage your groups" , reply_markup=InlineKeyboardMarkup([ 
+    bot.send_photo(message.chat.id ,photo="https://telegra.ph/file/0f4b6a40dc4eb52587234.jpg",caption="Hello there i'm Yuuki-San\nI'll help you to manage your groups" , reply_markup=InlineKeyboardMarkup([ 
         [InlineKeyboardButton('[ help ]' , callback_data="help")]
     ]))
