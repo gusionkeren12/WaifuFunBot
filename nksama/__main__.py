7f1c19c7da11e9d290c862fc9ba87b1ff34244bd
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-09 15:40:06 +0530

    Update __main__.py

--- nksama/__main__.py
+++ nksama/__main__.py
@@ -4,5 +4,5 @@ from nksama import bot
 
 if __name__ == "__main__":
     bot.run()
-    bot.send_message(-1001590378481 , "YuukiRobot Alive!")
+    bot.send_message("@unitedsupport" , "YuukiRobot Alive!")
     
