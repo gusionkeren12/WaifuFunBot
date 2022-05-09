dcd9a682ffe07e1f710680fc69e724a89d122cdc
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-10 01:09:42 +0530

    Update waifu.py

--- nksama/plugins/waifu.py
+++ nksama/plugins/waifu.py
@@ -3,16 +3,15 @@ import requests
 from nksama import bot 
 from pyrogram import filters
 
-
-bot.on. message(filters.command('waifu'))
-def waifu(_,message):
-     reply = message.reply_to_message
-    if reply:
-        res = requests.get('"https://api.waifu.pics/sfw/waifu').json()
-        url = res['link']
-        reply.reply_photo(url)
-          
-       else:
-         message.reply_animation(url)
         
     
+@bot.on_message(filters.command('waifu'))
+def waifu(_,message):
+    reply = message.reply_to_message
+    if reply:
+        res = requests.get('https://api.waifu.pics/sfw/waifu').json()
+        url = res['link']
+        reply.reply_photo(url)
+        
+    else:
+        message.reply_photo(url)
