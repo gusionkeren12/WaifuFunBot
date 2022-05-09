78a08144bccbd3cc4ab276ccd45af921e4223ecf
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-10 01:04:06 +0530

    Update waifu.py

--- nksama/plugins/waifu.py
+++ nksama/plugins/waifu.py
@@ -6,6 +6,10 @@ from pyrogram import filters
 
 bot.on. message(filters.command('waifu'))
 def waifu(_,message):
-    res = requests.get('https://some-random-api.ml/animu/pat').json()
-      
-  await bot.send_photo(message.chat.id, photo=e["url"], caption="Waifu images 💕💕")
+     reply = message.reply_to_message
+    if reply:
+        res = requests.get('"https://api.waifu.pics/sfw/waifu').json()
+        url = res['link']
+        reply.reply_photo(url)
+        
+    
