2d447c99d3950789364f5cac7585125c5eb77069
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-10 16:20:32 +0530

    Create db.py

---
+++ nksama/db.py
@@ -0,0 +1,9 @@
+import pymongo
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+
+MONGO_URL = "mongodb+srv://Music:Music@cluster0.f9x4i.mongodb.net/Cluster0?retryWrites=true&w=majority"
+
+log.info("Initializing MongoDB client")
+database = pymongo.MongoClient(MONGO_URL)["notes"]["notes"]
+mongo_client = MongoClient(MONGO_URL)
+db = mongo_client.nanamori
