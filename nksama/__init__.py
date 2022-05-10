ae0a68b5d6f78fd55b14e8fd1ff8334037019888
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-10 20:12:27 +0530

    Add files via upload

--- nksama/__init__.py
+++ nksama/__init__.py
@@ -1,1 +1,21 @@
+from pyrogram import filters , Client
+import os 
 
+from nksama.config import API_ID, API_HASH, BOT_TOKEN
+
+class Log:
+    def __init__(self, save_to_file=False, file_name="nanamori.log"):
+        self.save_to_file = save_to_file
+        self.file_name = file_name
+
+bot = Client(
+    'bot',
+    api_id=os.environ.get('API_ID'),
+    api_hash=os.environ.get('API_HASH'),
+    bot_token=os.environ.get('BOT_TOKEN'),
+    plugins=dict(root=f"{__name__}/plugins")
+)
+
+help_message = []
+
+dev_user = [1491497760,5145883564, 5175767264]
---
+++ nksama/__main__.py
@@ -0,0 +1,21 @@
+from nksama import bot
+import asyncio
+import logging
+import uvloop
+from nksama.config import SUPPORT_CHAT
+
+loop = asyncio.get_event_loop()
+
+logging.basicConfig(
+    filename="logs.txt",
+    level=logging.INFO,
+    filemode="a",
+    format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
+    datefmt="%H:%M:%S",
+)
+
+if __name__ == "__main__":
+    bot.run()
+    
+    bot.send_message(f"{SUPPORT_CHAT}" , "@WaifuFunBot Alive!")
+    
