a281bebdb85b63425a7ee8dbcac1c99c4269d381
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-10 16:44:43 +0530

    Create __main__.py

---
+++ nksama/__main__.py
@@ -0,0 +1,9 @@
+from nksama import bot
+from nksama.config import SUPPORT_CHAT
+
+
+if __name__ == "__main__":
+    bot.run()
+    
+    bot.send_message(f"{SUPPORT_CHAT}" , "@WaifuFunBot Alive!")
+    
