e5894e913034aedabbb8bcb0528fc4b18d6360e7
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-10 16:45:36 +0530

    Create __init__.py

---
+++ nksama/__init__.py
@@ -0,0 +1,14 @@
+from pyrogram import filters , Client
+import os 
+
+from nksama.config import API_ID, API_HASH, BOT_TOKEN
+
+bot = Client(
+    'bot',
+    api_id=os.environ.get('API_ID'),
+    api_hash=os.environ.get('API_HASH'),
+    bot_token=os.environ.get('BOT_TOKEN'),
+    plugins=dict(root=f"{__name__}/plugins")
+)
+
+help_message = []
