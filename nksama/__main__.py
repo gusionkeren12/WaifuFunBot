d592aca90b3e73ec315e42413f2605421b81941f
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-10 17:35:20 +0530

    Update __main__.py

--- nksama/__main__.py
+++ nksama/__main__.py
@@ -1,6 +1,15 @@
 from nksama import bot
 from nksama.config import SUPPORT_CHAT
 
+loop = asyncio.get_event_loop()
+
+logging.basicConfig(
+    filename="logs.txt",
+    level=logging.INFO,
+    filemode="a",
+    format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
+    datefmt="%H:%M:%S",
+)
 
 if __name__ == "__main__":
     bot.run()
