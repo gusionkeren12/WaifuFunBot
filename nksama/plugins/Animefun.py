d34555d3d6be39e8660e1e464c292c5f073a5648
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-10 11:36:28 +0530

    Update Animefun.py

--- nksama/plugins/Animefun.py
+++ nksama/plugins/Animefun.py
@@ -8,8 +8,28 @@ from pyrogram.types import Message
 from nksama.plugins.helpers import call_back_in_filter
 
 
-
-
+@bot.on_message(filters.command('wink'))
+def wink(_,message):
+    reply = message.reply_to_message
+    if reply:
+        res = requests.get('https://some-random-api.ml/animu/wink').json()
+        url = res['link']
+        reply.reply_animation(url)
+        
+    else:
+        message.reply_animation(url)
+        
+        
+@bot.on_message(filters.command('hug'))
+def hug(_,message):
+    reply = message.reply_to_message
+    if reply:
+        res = requests.get('https://some-random-api.ml/animu/hug').json()
+        url = res['link']
+        reply.reply_animation(url)
+        
+    else:
+        message.reply_animation(url)
 
 @bot.on_message(filters.command('pat'))
 def pat(_,message):
