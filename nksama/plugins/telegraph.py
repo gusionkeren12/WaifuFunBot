a4e56bb1de73210b184120923a839d67076c70ce
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-10 23:21:16 +0530

    Update telegraph.py

--- nksama/plugins/telegraph.py
+++ nksama/plugins/telegraph.py
@@ -2,15 +2,6 @@ from pyrogram import filters
 from pyrogram.types import Message
 from pyrogram.types.bots_and_keyboards.inline_keyboard_button import InlineKeyboardButton
 from pyrogram.types.bots_and_keyboards.inline_keyboard_markup import InlineKeyboardMarkup
-
-
-
-@bot.on_message(filters.command('start'))
-def start(_,message):
-
-    bot.send_photo(message.chat.id ,photo="https://telegra.ph/file/0f4b6a40dc4eb52587234.jpg",caption="Hello there i'm Yuuki-San\nI'll help you to manage your groups" , reply_markup=InlineKeyboardMarkup([ 
-        [InlineKeyboardButton('[ help ]' , callback_data="help")]
-    ]))
 from nksama import bot as app, telegraph
 
 
