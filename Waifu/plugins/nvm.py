31dda25863f482ca8a4f27d935bb8e99e1d437ca
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-10 20:08:48 +0530

    Delete nvm.py

--- Waifu/plugins/nvm.py
+++
@@ -1,8 +0,0 @@
-from Waifu import bot
-from pyrogram import filters
-
-
-@bot.on_message(filters.text & filters.chat(-1001590378481))
-def nevermind(_,message):
-  if message.from_user.id == 1491497760 and "punda" in message.text:
-    message.delete()
