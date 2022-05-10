26e458d2a6e1e20804e783f46c52f86f0ab2b52b
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-10 13:12:52 +0530

    Update __init__.py

--- nksama/__init__.py
+++ nksama/__init__.py
@@ -6,8 +6,8 @@ from nksama.config import API_ID, API_HASH, BOT_TOKEN
 bot = Client(
     'bot',
     api_id=os.environ.get('API_ID'),
-    api_hash=os.environ['API_HASH'],
-    bot_token=os.environ['BOT_TOKEN'],
+    api_hash=os.environ.get('API_HASH'),
+    bot_token=os.environ.get('BOT_TOKEN'),
     plugins=dict(root=f"{__name__}/plugins")
 )
 
