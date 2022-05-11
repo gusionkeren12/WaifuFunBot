41cc024d968e09c67076ce01bd88b36554613d98
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-11 22:21:35 +0530

    Update and rename admin.py to group_tools.py

--- nksama/plugins/group_tools.py
+++ nksama/plugins/group_tools.py
@@ -9,7 +9,7 @@ from nksama import help_message
 def start(_,message):
 
     bot.send_photo(message.chat.id , photo="https://telegra.ph/file/14f6ced5fa87e62b66977.jpg", caption="help and commands!", reply_markup=InlineKeyboardMarkup([ 
-        [InlineKeyboardButton('[ help ]' , callback_data="help")]
+        [InlineKeyboardButton('[ help ]' , callback_data="vegeta_admin")]
     ]))
     
     
@@ -22,6 +22,20 @@ def ids(_,message):
     message.reply(f"**Your id**: {message.from_user.id}\n**chat id**: {message.chat.id}")
 
     
+elif query.data == "vegeta_admin":
+        query.message.edit_caption(
+            "*๏ Let's make your group bit effective now*"
+            "\nCongragulations, VegetaRobot now ready to manage your group."
+            "\n\n*Admin Tools*"
+            "\nBasic Admin tools help you to protect and powerup your group."
+            "\nYou can ban members, Kick members, Promote someone as admin through commands of bot."
+            "\n\n*Greetings*"
+            "\nLets set a welcome message to welcome new users coming to your group."
+            "\nsend `/setwelcome [message]` to set a welcome message!",
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton('WoW', url="t.me/vegetarobot")]]
+            ),
+        )
     
 help_message.append({
     "Module_Name": "admin" ,
