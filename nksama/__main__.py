0b9c07f94f210c0016e2ac36fa98c18b62174d5d
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-11 22:58:46 +0530

    Update __main__.py

--- nksama/__main__.py
+++ nksama/__main__.py
@@ -14,5 +14,5 @@ logging.basicConfig(
 
 if __name__ == "__main__":
     bot.run()
-   with bot:
-    bot.send_message(f"{SUPPORT_CHAT}", "Bot Started!!")
+    with bot:
+        bot.send_message(f"{SUPPORT_CHAT}" , "Hello there I'm Now online")
