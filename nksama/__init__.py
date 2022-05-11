d460a87eb25acf577f82cf0c0f43cb5eb4be8e00
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-11 18:21:56 +0530

    Update __init__.py

--- nksama/__init__.py
+++ nksama/__init__.py
@@ -3,15 +3,22 @@ import os , time
 from telegraph import Telegraph
 StartTime = time.time()
 
-from nksama.config import API_ID, API_HASH, BOT_TOKEN
+from nksama.config import *
 
 
 #config vars
-BOT_USERNAME = os.environ.get("BOT_USERNAME", None)
 API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 bot = Client("nksama", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="{}/plugins".format(__name__)))
+
+x = bot.get_me()
+
+BOT_ID = x.id
+BOT_NAME = x.first_name + (x.last_name or "")
+BOT_USERNAME = x.username
+BOT_MENTION = x.mention
+BOT_DC_ID = x.dc_id
 
 print("Bot is Working")
 
@@ -23,4 +30,5 @@ telegraph.create_account(short_name=BOT_USERNAME)
 
 dev_user = [1491497760, 5337900484]
 
-LOG_GROUP_ID = "-1001690512977"
+#from nksama config vars
+LOG_GROUP_ID = LOG_GROUP_ID
