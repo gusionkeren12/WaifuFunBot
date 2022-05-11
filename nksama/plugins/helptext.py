097d6af6038ef69e799b4a2f242f337e0c4c49cb
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-11 12:59:54 +0530

    Update helptext.py

--- nksama/plugins/helptext.py
+++ nksama/plugins/helptext.py
@@ -8,14 +8,14 @@ class Help_Text:
 • /rmeme - __to get random anime memes__
 """,
  
-       "grouptools_help" : """
+       "group_tools_help" : """
 • /id - __Get User & Chat ID's__
 • /ban - __Reply to a user BAN__
 • /unban __User id or username UNBAN__
 • /pin - __Reply to a message PIN__
 • /unpin - __Reply to a message UNPIN__
 • /invitelink - __Get invitelink of the current chat__
-""",
+"""
         
     
     }
