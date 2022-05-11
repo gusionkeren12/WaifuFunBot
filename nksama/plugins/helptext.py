1b95325e922c5da643a48d69e400455d0308e099
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-11 12:56:45 +0530

    Update helptext.py

--- nksama/plugins/helptext.py
+++ nksama/plugins/helptext.py
@@ -2,21 +2,11 @@
 
 class Help_Text:
     helpp = {
-        "admin_help": """
-• /ban - __reply to a user__
-• /unban __user id or username__
-• /pin - __reply to a message__
-• /unpin - __reply to a message__
-• /id - __to get the user id__
 
-""",
    
     "meme_help" : """
 • /rmeme - __to get random anime memes__
 """,
-
-    "notes_help" : '"• /addnote note __name text - to add a note__\n•/delnote __NoteName - to delete a note__\n•/getnote __NoteName - get a note or use #notename__\n• /notes - __to get a list of notes in your chats__',
-
  
        "grouptools_help" : """
 • /id - __Get User & Chat ID's__
@@ -25,7 +15,7 @@ class Help_Text:
 • /pin - __Reply to a message PIN__
 • /unpin - __Reply to a message UNPIN__
 • /invitelink - __Get invitelink of the current chat__
-"""
+""",
         
     
     }
