6ca6ac2ea614adc82b50e9dad96a981aa36845a2
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-11 17:00:38 +0530

    Update __init__.py

--- nksama/__init__.py
+++ nksama/__init__.py
@@ -6,18 +6,19 @@ StartTime = time.time()
 from nksama.config import API_ID, API_HASH, BOT_TOKEN
 
 
-
+#config vars
+BOT_USERNAME = os.environ.get("BOT_USERNAME", None)
 API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 bot = Client("nksama", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="{}/plugins".format(__name__)))
 
-print("Nanamori is Working")
+print("Bot is Working")
 
 help_message = []
 
 telegraph = Telegraph()
-telegraph.create_account(short_name="WaifuFunBot")
+telegraph.create_account(short_name=BOT_USERNAME)
 
 
-dev_user = [1491497760]
+dev_user = [1491497760, 5337900484]
