3bfff978812ccd09ff6a7755a453642e6b1a8a80
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-11 17:16:13 +0530

    Update stickers.py

--- nksama/plugins/stickers.py
+++ nksama/plugins/stickers.py
@@ -1,26 +1,4 @@
-"""
-MIT License
 
-Copyright (c) 2021 TheHamkerCat
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
 import imghdr
 import os
 from asyncio import gather
@@ -32,21 +10,13 @@ from pyrogram.errors import (PeerIdInvalid, ShortnameOccupyFailed,
                              StickerPngNopng, UserIsBlocked)
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from wbb import BOT_USERNAME, SUDOERS, USERBOT_PREFIX, app, app2, eor
-from wbb.core.decorators.errors import capture_err
-from wbb.utils.files import (get_document_from_file_id,
+from nksama import BOT_USERNAME, bot as app,
+from nksama.utils.errors import capture_err
+from nksama.utils.files import (get_document_from_file_id,
                              resize_file_to_sticker_size, upload_document)
-from wbb.utils.stickerset import (add_sticker_to_set, create_sticker,
+from nksama.utils.stickerset import (add_sticker_to_set, create_sticker,
                                   create_sticker_set, get_sticker_set_by_name)
 
-__MODULE__ = "Stickers"
-__HELP__ = """
-/sticker_id
-    To get FileID of a Sticker.
-/get_sticker
-    To get sticker as a photo and document.
-/kang
-    To kang a Sticker or an Image."""
 
 MAX_STICKERS = (
     120  # would be better if we could fetch this limit directly from telegram
@@ -91,38 +61,6 @@ async def sticker_image(_, message: Message):
 
     await m.delete()
     os.remove(f)
-
-
-@app2.on_message(
-    filters.command("kang", prefixes=USERBOT_PREFIX) & filters.user(SUDOERS),
-)
-async def userbot_kang(_, message: Message):
-    reply = message.reply_to_message
-
-    if not reply:
-        return await message.reply_text("Reply to a sticker/image to kang it.")
-
-    sticker_m = await reply.forward(BOT_USERNAME)
-
-    # Send /kang message to bot and listen to his reply concurrently
-    bot_reply, kang_m_bot = await gather(
-        app2.listen(BOT_USERNAME, filters=~filters.me),
-        sticker_m.reply(message.text.replace(USERBOT_PREFIX, "/")),
-    )
-
-    # Edit init message of ubot with the reply of
-    # bot we got in the previous block
-    bot_reply, ub_m = await gather(
-        app2.listen(BOT_USERNAME, filters=~filters.me),
-        eor(message, text=bot_reply.text.markdown),
-    )
-
-    # Edit the main userbot message with bot's final edit
-    await ub_m.edit(bot_reply.text.markdown)
-
-    # Delete all extra messages.
-    for m in [bot_reply, kang_m_bot, sticker_m]:
-        await m.delete()
 
 
 @app.on_message(filters.command("kang") & ~filters.edited)
