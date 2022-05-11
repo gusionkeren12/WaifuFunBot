3aa00cba67ead33f1aa7c9786574a362bfde17a0
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-11 15:32:37 +0530

    Update dev_user.py

--- nksama/plugins/dev_user.py
+++ nksama/plugins/dev_user.py
@@ -190,4 +190,7 @@ async def ping(_, m):
     end_time = time.time()
     ping_time = round((end_time - start_time) * 1000, 3)
     uptime = get_readable_time((time.time() - StartTime))
+    m.delete()
     await text.edit_text(f"**🏓 PONG!!:** `{ping_time} ms`\n**🆙 UPTIME:** `{uptime}`")
+    
+    
