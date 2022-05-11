a03160c928eac8703887701efba76381ef40b687
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-11 20:07:14 +0530

    Create admin.py

---
+++ nksama/plugins/admin.py
@@ -0,0 +1,33 @@
+from pyrogram.types.bots_and_keyboards.inline_keyboard_button import InlineKeyboardButton
+from pyrogram.types.bots_and_keyboards.inline_keyboard_markup import InlineKeyboardMarkup
+from nksama import bot
+from pyrogram import filters 
+from nksama import help_message
+
+
+@bot.on_message(filters.command('start'))
+def start(_,message):
+
+    bot.send_photo(message.chat.id , photo="https://telegra.ph/file/14f6ced5fa87e62b66977.jpg", caption="help and commands!", reply_markup=InlineKeyboardMarkup([ 
+        [InlineKeyboardButton('[ help ]' , callback_data="help")]
+    ]))
+    
+    
+@bot.on_message(filters.command('id'))
+def ids(_,message):
+  reply = message.reply_to_message
+  if reply:
+    message.reply_text(f"**Your id**: {message.from_user.id}\n**User id**: {reply.from_user.id}\n**chat id**: {message.chat.id}")
+  else:
+    message.reply(f"**Your id**: {message.from_user.id}\n**chat id**: {message.chat.id}")
+
+    
+    
+help_message.append({
+    "Module_Name": "admin" ,
+    "Help": """
+/ban - reply to a user
+/unban user id or username
+/pin - reply to a message
+/unpin - reply to a message
+"""})  
