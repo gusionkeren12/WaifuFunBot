647243657be47cfb264ce86e19ae34f984d98e00
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-12 23:24:48 +0530

    Update config.py

--- nksama/config.py
+++ nksama/config.py
@@ -3,8 +3,8 @@ API_ID = 7126006
 API_HASH = "f92b05be529835381859ead64a195fa2"
 BOT_TOKEN = "5380977231:AAHhb9dBAeVm8wsDFk9YY1a_NT6sEiIePys"
 BOT_USERNAME = "VegetaRobot"
-UPDATES_CHANNEL = "VegetaUpdates"
-SUPPORT_CHAT = "vegetasupport"
+UPDATES_CHANNEL = "@VegetaUpdates"
+SUPPORT_CHAT = "@vegetasupport"
 LOG_GROUP_ID = "-1001690512977"
 WALL_API_KEY = "awoo"
 
