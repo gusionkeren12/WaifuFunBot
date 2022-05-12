82ac3572a400b3eb143546f7fb9dfd4df2db542f
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-12 23:44:54 +0530

    Update start_help_text.py

--- nksama/plugins/start_help_text.py
+++ nksama/plugins/start_help_text.py
@@ -4,14 +4,15 @@ from pyrogram.types.bots_and_keyboards.inline_keyboard_button import InlineKeybo
 from pyrogram.types.bots_and_keyboards.inline_keyboard_markup import InlineKeyboardMarkup
 from nksama import bot
 
-
+BOT_IMG = [ "https://telegra.ph/file/b3fbf990e0b67ede241a3.jpg",
+           "https://telegra.ph/file/94865dae2576a2fa52732.jpg" ]
 
 @bot.on_message(filters.command(["start"], ["/", ".", "?"]))
 async def start(bot, m: Message):
     buttons = [
         [
             InlineKeyboardButton(
-                "ADD ME", switch_inline_query_current_chat=""
+                "ADD ME", url="t.me/VegetaRobot?startgroup=true"
             ),
             InlineKeyboardButton(
                 "Support", url=f"{SUPPORT_CHAT}"
