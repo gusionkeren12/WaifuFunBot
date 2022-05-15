23dc9e5f34ab36c58e5f78cdac0a69fce2944594
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-16 00:11:51 +0530

    Update group_tools.py

--- nksama/plugins/group_tools.py
+++ nksama/plugins/group_tools.py
@@ -10,8 +10,8 @@ from nksama import help_message
 def ids(_,message):
   reply = message.reply_to_message
   if reply:
-    message.reply_text(f"**Your id**: {message.from_user.id}\n**User id**: {reply.from_user.id}\n**chat id**: {message.chat.id}")
+    message.reply_text(f"**{message.from_user.mention}**: `{message.from_user.id}´\n**{reply.from_user.mention}**: {reply.from_user.id}\n**chat id**: {message.chat.id}")
   else:
-    message.reply(f"**Your id**: {message.from_user.id}\n**chat id**: {message.chat.id}")
+    message.reply(f"**{message.from_user. mention}**: {message.from_user.id}\n**chat id**: {message.chat.id}")
 
    
