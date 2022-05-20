f1814a12dd2080e22f393f2cf0349d9c38a7ebce
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-20 07:56:57 +0530

    Delete nvm.py

--- nksama/plugins/nvm.py
+++
@@ -1,8 +0,0 @@
-from nksama import bot
-from pyrogram import filters
-
-
-@bot.on_message(filters.text & filters.chat("@unitedsupport"))
-def nevermind(_,message):
-  if message.from_user.id == 1491497760 and "completed" in message.text:
-    message.delete()
