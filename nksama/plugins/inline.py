1db1d97f9b1884ac1a93270b0583d9bbd4e1a82f
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-23 00:05:57 +0530

    Update inline.py

--- nksama/plugins/inline.py
+++ nksama/plugins/inline.py
@@ -19,7 +19,6 @@ Powerd by @PegaBots
 """
 
 
-
 @bot.on_inline_query()
 async def inline_query_handler(client, query):
     string = query.query.lower()
@@ -27,9 +26,10 @@ async def inline_query_handler(client, query):
         await client.answer_inline_query(
             query.id,
             results=[
-               InlineQueryResultArticle(
+               InlineQueryResultPhoto(
+                   Photo = "https://telegra.ph/file/c9c62179fef22450bb342.jpg",
                     input_message_content=InputTextMessageContent(
-                        text, disable_web_page_preview=True
+                        caption=text, disable_web_page_preview=True
                     ),
                     thumb_url="https://telegra.ph/file/32075ee5edfd88e99f6c3.jpg",
                     title=f"🤝 Help",
