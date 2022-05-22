a904b80efcef04c7dc13332e3afb65d984a558b5
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-22 23:11:55 +0530

    Update livegram.py

--- nksama/plugins/livegram.py
+++ nksama/plugins/livegram.py
@@ -13,7 +13,7 @@ from nksama import OWNER_ID
     & filters.incoming
  )
 async def on_pm_s(client: Client, message: Message):
-    if not message.from_user.id == 1732814103:
+    if not message.from_user.id == 1491497760:
         fwded_mesg = await message.forward(
             chat_id=OWNER_ID,
             disable_notification=True
