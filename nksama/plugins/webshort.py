2d16885e1f27b0f7ad05ed2bd193562f393d20a8
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-23 22:29:14 +0530

    Update webshort.py

--- nksama/plugins/webshort.py
+++ nksama/plugins/webshort.py
@@ -6,23 +6,65 @@ from nksama import bot as app
 from nksama.utils.errors import capture_err
 
 
+ 
+# Translate
+@app.on_message(filters.command("tr") & ~filters.edited)
+@capture_err
+async def tr(_, message):
+    if len(message.command) != 2:
+        return await message.reply_text("/tr [LANGUAGE_CODE]")
+    lang = message.text.split(None, 1)[1]
+    if not message.reply_to_message or not lang:
+        return await message.reply_text(
+            "Reply to a message with /tr [language code]"
+            + "\nGet supported language list from here -"
+            + " https://py-googletrans.readthedocs.io/en"
+            + "/latest/#googletrans-languages"
+        )
+    reply = message.reply_to_message
+    text = reply.text or reply.caption
+    if not text:
+        return await message.reply_text("Reply to a text to translate it")
+    result = await arq.translate(text, lang)
+    if not result.ok:
+        return await message.reply_text(result.result)
+    await message.reply_text(result.result.translatedText)
+
+
+@app.on_message(filters.command("json") & ~filters.edited)
+@capture_err
+async def json_fetch(_, message):
+    if len(message.command) != 2:
+        return await message.reply_text("/json [URL]")
+    url = message.text.split(None, 1)[1]
+    m = await message.reply_text("Fetching")
+    try:
+        data = await get(url)
+        data = await json_prettify(data)
+        if len(data) < 4090:
+            await m.edit(data)
+        else:
+            link = await paste(data)
+            await m.edit(
+                f"[OUTPUT_TOO_LONG]({link})",
+                disable_web_page_preview=True,
+            )
+    except Exception as e:
+        await m.edit(str(e))
+
 
 @app.on_message(filters.command("webss"))
 @capture_err
-async def take_ss(_, message: Message):
+async def take_ss(_, message):
+    if len(message.command) != 2:
+        return await message.reply_text("Give A Url To Fetch Screenshot.")
+    url = message.text.split(None, 1)[1]
+    m = await message.reply_text("**Uploading**")
     try:
-        if len(message.command) != 2:
-            return await message.reply_text("Give A Url To Fetch Screenshot.")
-        url = message.text.split(None, 1)[1]
-        m = await message.reply_text("**Taking Screenshot**")
-        await m.edit("**Uploading**")
-        try:
-            await message.reply_photo(
-                photo=f"https://webshot.amanoteam.com/print?q={url}",
-                quote=False,
-            )
-        except TypeError:
-            return await m.edit("No Such Website.")
-        await m.delete()
-    except Exception as e:
-        await message.reply_text(str(e))
+        await app.send_photo(
+            message.chat.id,
+            photo=f"https://webshot.amanoteam.com/print?q={url}",
+        )
+    except Exception:
+        return await m.edit("No Such Website.")
+    await m.delete()
