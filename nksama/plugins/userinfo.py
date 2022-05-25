544e164e0f8330687bc10e924cffca1e7e97d9d2
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-25 22:43:13 +0530

    Update userinfo.py

--- nksama/plugins/userinfo.py
+++ nksama/plugins/userinfo.py
@@ -1,10 +1,7 @@
 import os
 
-from pyrogram import filters,Client
+from pyrogram import filters
 from pyrogram.types import Message
-
-from pyrogram.types.messages_and_media import message
-
 from nksama import dev_user, bot
 from nksama.utils.sections import section
 
@@ -120,59 +117,3 @@ async def chat_info_func(_, message: Message):
         await m.edit(e)
 
 
-
-
-def get_file_id(msg: Message):
-    if msg.media:
-        for message_type in (
-            "photo",
-            "animation",
-            "audio",
-            "document",
-            "video",
-            "video_note",
-            "voice",
-            # "contact",
-            # "dice",
-            # "poll",
-            # "location",
-            # "venue",
-            "sticker",
-        ):
-            obj = getattr(msg, message_type)
-            if obj:
-                setattr(obj, "message_type", message_type)
-                return obj
-    
-    
-@bot.on_message(filters.command('id'))
-async def id(_, message):
-    chat_type = message.chat.type
-
-    if chat_type == "private":
-        user_id = message.chat.id
-        await message.reply_text(f"<code>{user_id}</code>", quote=True)
-
-    elif chat_type in ["group", "supergroup"]:
-        _id = ""
-        _id += "<b>Chat ID</b>: " f"<code>{message.chat.id}</code>\n"
-        if message.reply_to_message:
-            _id += (
-                "<b>Replied User ID</b>: "
-                f"<code>{message.reply_to_message.from_user.id}</code>\n"
-            )
-            file_info = get_file_id(message.reply_to_message)
-            if file_info:
-                _id += (
-                    f"<b>{file_info.message_type}</b>: "
-                    f"<code>{file_info.file_id}</code>\n"
-                )
-        else:
-            _id += "<b>User ID</b>: " f"<code>{message.from_user.id}</code>\n"
-            file_info = get_file_id(message)
-            if file_info:
-                _id += (
-                    f"<b>{file_info.message_type}</b>: "
-                    f"<code>{file_info.file_id}</code>\n"
-                )
-        await message.reply_text(_id, quote=True)
