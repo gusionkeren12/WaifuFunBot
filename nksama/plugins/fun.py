69525e3b73232f6ed159e1fc582dd559c95f7eda
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-25 09:57:33 +0530

    Update fun.py

--- nksama/plugins/fun.py
+++ nksama/plugins/fun.py
@@ -1,10 +1,11 @@
 from pyrogram.types import Message
 from pyrogram import filters
+from nksama import bot
 
 deathnote_img = "https://telegra.ph/file/9e220eb59606f8435eafe.mp4"
 
 @bot.on_message(filters.command('deathnote'))
-def deathnote(_, m: message):
+def deathnote(_, m: Message):
     reply = m.reply_to_message
     if reply:
         reply.reply_animation(deathnote_img,
