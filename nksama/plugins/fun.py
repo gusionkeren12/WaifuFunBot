0960729d39fe254aa1ce0a159835b88e2e14fc2b
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-25 16:51:22 +0530

    Update fun.py

--- nksama/plugins/fun.py
+++ nksama/plugins/fun.py
@@ -2,12 +2,15 @@ from pyrogram.types import Message
 from pyrogram import filters
 from nksama import bot
 
+dnote = [ ]
+
 deathnote_img = "https://telegra.ph/file/9e220eb59606f8435eafe.mp4"
 
 @bot.on_message(filters.command('deathnote'))
 def deathnote(_, m: Message):
     reply = m.reply_to_message
     if reply:
+        dnote.append(reply.from_user.mention)
         reply.reply_animation(deathnote_img,
                               caption="{} Added Death Note List!".format(reply.from_user.mention))
         
