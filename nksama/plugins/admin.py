8f78a379c1c06383ce039e70c03738faf226d5d0
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-26 00:50:56 +0530

    Update admin.py

--- nksama/plugins/admin.py
+++ nksama/plugins/admin.py
@@ -11,3 +11,25 @@ def ban(_, m: Message):
     
     else:
         m.reply("only Dev use this")
+
+        
+@bot.on_message(filters.command('unban'))
+def ban(_, m: Message):
+    reply = m.reply_to_message
+    if m.from_user.id in dev_user:
+        bot.unban_chat_member(m.chat.id , reply.from_user.id)
+        bot.send_message(m.chat.id ,f"Unbanned! {reply.from_user.mention}")
+    
+    else:
+        m.reply("only Dev use this")
+
+        
+@bot.on_message(filters.command('kick'))
+def ban(_, m: Message):
+    reply = m.reply_to_message
+    if m.from_user.id in dev_user:
+        bot.ban_chat_member(m.chat.id , reply.from_user.id)
+        bot.send_message(m.chat.id ,f"Kicked! {reply.from_user.mention}")
+        bot.unban_chat_member(m.chat.id, reply.from_user.id)
+    else:
+        m.reply("only Dev use this")
