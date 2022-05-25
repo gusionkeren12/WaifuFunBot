a15ccc10f6a483c338deb4fa122b012f15a7f89a
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-25 09:51:37 +0530

    Update fun.py

--- nksama/plugins/fun.py
+++ nksama/plugins/fun.py
@@ -1,14 +1,16 @@
 from pyrogram.types import Message
 from pyrogram import filters
 
-@bot.on_message(filters.command('hmeme'))
-def hmeme(_, m: message):
+deathnote_img = "https://telegra.ph/file/9e220eb59606f8435eafe.mp4"
+
+@bot.on_message(filters.command('deathnote'))
+def deathnote(_, m: message):
     reply = m.reply_to_message
     if reply:
-        reply.reply_animation(random.choice(deathnote),
-                              caption="{} Added Death Note List!".format(m.reply_user.mention)
+        reply.reply_animation(deathnote_img,
+                              caption="{} Added Death Note List!".format(m.reply_user.mention))
         
     else:
-        m.reply_animation(random.choice(deathnote),
-                              caption="{} Added Death Note List!".format(m.from_user.mention)
+        m.reply_animation(deathnote_img,
+                              caption="{} Added Death Note List!".format(m.from_user.mention))
         
