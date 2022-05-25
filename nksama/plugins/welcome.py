3607f5c742a2a3d29332862fad1f931f0d8085f9
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-25 18:12:30 +0530

    Update welcome.py

--- nksama/plugins/welcome.py
+++ nksama/plugins/welcome.py
@@ -4,4 +4,4 @@ from nksama import bot
 
 @bot.on_message(filters.new_chat_members)
 async def welcome(_, m: Message):
-        await m.reply("hello! {} Welcome to **{}**".format(m.from_user.mention,m.chat.title)
+        await m.reply("hello! {} Welcome to **{}**".format(m.from_user.mention,m.chat.title))
