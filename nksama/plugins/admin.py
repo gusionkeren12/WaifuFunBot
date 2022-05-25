0ec4abef998e3aa55c3e4ca31db44da64aadb4d1
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-26 00:40:11 +0530

    Update admin.py

--- nksama/plugins/admin.py
+++ nksama/plugins/admin.py
@@ -3,8 +3,8 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 @bot.on_message(filters.command('ban'))
-def ban(_,m: Message):
-    reply = message.reply_to_message
+def ban(_, m: Message):
+    reply = m.reply_to_message
     if m.from_user.id in dev_user:
         bot.kick_chat_member(m.chat.id , reply.from_user.id)
         bot.send_message(m.chat.id ,f"Banned! {reply.from_user.mention}")
