e3f282ea2ccd9c41ca823c1254b7807d1441838a
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-25 21:57:09 +0530

    Update infos.py

--- nksama/plugins/infos.py
+++ nksama/plugins/infos.py
@@ -143,3 +143,36 @@ def get_file_id(msg: Message):
             if obj:
                 setattr(obj, "message_type", message_type)
                 return obj
+    
+    
+@bot.on_message(filters.command('id'))
+async def id(bot: client, message):
+    chat_type = message.chat.type
+
+    if chat_type == "private":
+        user_id = message.chat.id
+        await message.reply_text(f"<code>{user_id}</code>", quote=True)
+
+    elif chat_type in ["group", "supergroup"]:
+        _id = ""
+        _id += "<b>Chat ID</b>: " f"<code>{message.chat.id}</code>\n"
+        if message.reply_to_message:
+            _id += (
+                "<b>Replied User ID</b>: "
+                f"<code>{message.reply_to_message.from_user.id}</code>\n"
+            )
+            file_info = get_file_id(message.reply_to_message)
+            if file_info:
+                _id += (
+                    f"<b>{file_info.message_type}</b>: "
+                    f"<code>{file_info.file_id}</code>\n"
+                )
+        else:
+            _id += "<b>User ID</b>: " f"<code>{message.from_user.id}</code>\n"
+            file_info = get_file_id(message)
+            if file_info:
+                _id += (
+                    f"<b>{file_info.message_type}</b>: "
+                    f"<code>{file_info.file_id}</code>\n"
+                )
+        await message.reply_text(_id, quote=True)
