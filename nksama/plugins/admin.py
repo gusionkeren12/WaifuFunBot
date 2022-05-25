49734f1ba9abc6f51db581cb592976b3635393ee
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-26 00:16:31 +0530

    Update admin.py

--- nksama/plugins/admin.py
+++ nksama/plugins/admin.py
@@ -16,9 +16,10 @@ def is_admin(group_id: int, user_id: int):
         return False
 
 @bot.on_message(filters.command('ban'))
-def ban(_,message):
-    # scammer = reply.from_user.id
+def ban(_,m: message):
     reply = message.reply_to_message
-    if is_admin(message.chat.id , message.from_user.id) and reply and reply.from_user.id not in dev_user and message.from_user.id != 825664681:
-        bot.kick_chat_member(message.chat.id , message.reply_to_message.from_user.id)
-        bot.send_message(message.chat.id ,f"Banned! {reply.from_user.username}")
+    if m.from_user.id in dev_user:
+        bot.kick_chat_member(m.chat.id , reply.from_user.id)
+        bot.send_message(m.chat.id ,f"Banned! {reply.from_user.mention}")
+   else:
+       m.reply("only Dev use this")
