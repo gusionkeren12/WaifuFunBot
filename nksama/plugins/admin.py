cf23b9966255c2c1f2bfe6f01974a84d2064b134
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-26 14:13:41 +0530

    Update admin.py

--- nksama/plugins/admin.py
+++ nksama/plugins/admin.py
@@ -39,6 +39,7 @@ def kick(_, m: Message):
 @bot.on_message(filters.command('pin'))
 def pin(_, m: Message):
     reply = m.reply_to_message
+    
     if m.from_user.id in dev_user:
         bot.pin_chat_message(m.chat.id , reply.message_id)
         bot.send_message(m.chat.id ,f"pinned by {m.from_user.mention}!")
