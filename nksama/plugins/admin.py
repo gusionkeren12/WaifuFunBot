de726fd6e61a948c8447684451fd4e668964ed4d
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-26 07:52:44 +0530

    Update admin.py

--- nksama/plugins/admin.py
+++ nksama/plugins/admin.py
@@ -27,8 +27,8 @@ def unban(_, m: Message):
 @bot.on_message(filters.command('kick'))
 def kick(_, m: Message):
     reply = m.reply_to_message
-    if m.from_user.id in dev_user:
-        bot.ban_chat_member(m.chat.id , reply.from_user.id)
+          if m.from_user.id in dev_user:
+            bot.ban_chat_member(m.chat.id , reply.from_user.id)
         bot.send_message(m.chat.id ,f"Kicked! {reply.from_user.mention}")
         bot.unban_chat_member(m.chat.id, reply.from_user.id)
     else:
