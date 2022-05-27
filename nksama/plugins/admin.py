032a6115dd26ba5a50b3195c0cf63fd5e4eebba7
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-27 10:16:07 +0530

    Update admin.py

--- nksama/plugins/admin.py
+++ nksama/plugins/admin.py
@@ -1,4 +1,5 @@
-from nksama import bot, dev_user
+from nksama import bot as app, bot,  dev_user
+from nksama.config import BOT_ID
 from pyrogram import filters
 from pyrogram.types import Message
 
@@ -133,3 +134,44 @@ async def purgeFunc(_, message: Message):
         )
  
     
+@app.on_message(
+    filters.command(["promote", "fullpromote"]))
+@adminsOnly("can_promote_members")
+async def promoteFunc(_, message: Message):
+    user_id = await extract_user(message)
+    umention = (await app.get_users(user_id)).mention
+    if not user_id:
+        return await message.reply_text("I can't find that user.")
+    bot = await app.get_chat_member(message.chat.id, BOT_ID)
+    if user_id == BOT_ID:
+        return await message.reply_text("I can't promote myself.")
+    if not bot.can_promote_members:
+        return await message.reply_text("I don't have enough permissions")
+    if message.command[0][0] == "f":
+        await message.chat.promote_member(
+            user_id=user_id,
+            can_change_info=bot.can_change_info,
+            can_invite_users=bot.can_invite_users,
+            can_delete_messages=bot.can_delete_messages,
+            can_restrict_members=bot.can_restrict_members,
+            can_pin_messages=bot.can_pin_messages,
+            can_promote_members=bot.can_promote_members,
+            can_manage_chat=bot.can_manage_chat,
+            can_manage_voice_chats=bot.can_manage_voice_chats,
+        )
+        return await message.reply_text(f"Fully Promoted! {umention}")
+
+    await message.chat.promote_member(
+        user_id=user_id,
+        can_change_info=False,
+        can_invite_users=bot.can_invite_users,
+        can_delete_messages=bot.can_delete_messages,
+        can_restrict_members=False,
+        can_pin_messages=False,
+        can_promote_members=False,
+        can_manage_chat=bot.can_manage_chat,
+        can_manage_voice_chats=bot.can_manage_voice_chats,
+    )
+    await message.reply_text(f"Promoted! {umention}")
+
+
