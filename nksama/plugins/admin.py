f3065ffbd0c13b4888c9e1bff38e1aeeb7402f5d
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-27 07:57:59 +0530

    Update admin.py

--- nksama/plugins/admin.py
+++ nksama/plugins/admin.py
@@ -71,6 +71,18 @@ def unpin(_, m: Message):
         bot.send_message(m.chat.id ,f"unpinned by {m.from_user.mention}!")
     
     else:
-        m.reply("`only Dev use this`")
+       m.reply("`only Dev use this`")
+    
+@bot.on_message(filters.command('promote')) 
+def promote(_,message):
+    if is_admin(message.chat.id , message.from_user.id) and message.reply_to_message:
+       message.chat.promote_member(message.reply_to_message.from_user.id)
+       message.reply('Promoted @{} !'.format(message.reply_to_message.from_user.username))
+
+@bot.on_message(filters.command('demote')) 
+def demote(_,message):
+    if is_admin(message.chat.id , message.from_user.id) and message.reply_to_message:
+       message.chat.promote_member(message.reply_to_message.from_user.id, False,False,False,False,False,False,False,False)
+       message.reply('Demoted @{} !'.format(message.reply_to_message.from_user.username))
 
         
