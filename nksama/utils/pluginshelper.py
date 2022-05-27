e663e568a6870163f99c6c73045a0dda25990181
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-27 11:16:22 +0530

    Update pluginshelper.py

--- nksama/utils/pluginshelper.py
+++ nksama/utils/pluginshelper.py
@@ -1,2 +1,43 @@
+
+
+async def extract_user_and_reason(message, sender_chat=False):
+    args = message.text.strip().split()
+    text = message.text
+    user = None
+    reason = None
+    if message.reply_to_message:
+        reply = message.reply_to_message
+        # if reply to a message and no reason is given
+        if not reply.from_user:
+            if (
+                reply.sender_chat
+                and reply.sender_chat != message.chat.id
+                and sender_chat
+            ):
+                id_ = reply.sender_chat.id
+            else:
+                return None, None
+        else:
+            id_ = reply.from_user.id
+
+        if len(args) < 2:
+            reason = None
+        else:
+            reason = text.split(None, 1)[1]
+        return id_, reason
+
+    # if not reply to a message and no reason is given
+    if len(args) == 2:
+        user = text.split(None, 1)[1]
+        return await extract_userid(message, user), None
+
+    # if reason is given
+    if len(args) > 2:
+        user, reason = text.split(None, 2)[1:]
+        return await extract_userid(message, user), reason
+
+    return user, reason
+
+
 async def extract_user(message):
     return (await extract_user_and_reason(message))[0]
