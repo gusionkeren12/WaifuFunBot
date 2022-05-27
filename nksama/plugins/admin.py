2e7abd334ff806735fe7423381d9f527ad2a4b15
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-27 11:38:21 +0530

    Update admin.py

--- nksama/plugins/admin.py
+++ nksama/plugins/admin.py
@@ -140,7 +140,7 @@ async def purgeFunc(_, message: Message):
 @adminsOnly("can_promote_members")
 async def promoteFunc(_, message: Message):
     user_id = await extract_user(message)
-    umention = (await app.get_users(user_id)).mention
+    umention = (await bot.get_users(user_id)).mention
     if not user_id:
         return await message.reply_text("I can't find that user.")
     bot = await app.get_chat_member(message.chat.id, BOT_ID)
