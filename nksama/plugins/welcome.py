affc0af4aee04ce92e912ddbb15856f789d8c30b
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-27 20:16:00 +0530

    Update welcome.py

--- nksama/plugins/welcome.py
+++ nksama/plugins/welcome.py
@@ -2,9 +2,11 @@ from pyrogram import filters
 from pyrogram.types import Message
 from nksama import bot
 
+WEL_GIF = "https://telegra.ph/file/b616d3a126715d9b1aa46.mp4"
+
 @bot.on_message(filters.new_chat_members)
 async def welcome(_, m: Message):
-        await m.reply("hello! dear {}\nWelcome to **{}**".format(m.from_user.mention,m.chat.title))
+        await m.reply_photo(WEL_GIF,"hello! dear {}\nWelcome to **{}**".format(m.from_user.mention,m.chat.title))
         
 @bot.on_message(filters.left_chat_member)
 async def left(_, m: Message):
