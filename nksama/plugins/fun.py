ee48a34bd965dd35a1d7735e8386946dfd4db9ab
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-28 22:59:09 +0530

    Update fun.py

--- nksama/plugins/fun.py
+++ nksama/plugins/fun.py
@@ -90,7 +90,7 @@ async def gbams(_, m: Message):
       if reply.from_user.id == BOT_ID:
           return await m.reply_text("nigga I can't gban myself")
    else:
-        gbam = await m.reply("Gbaning...")
+         gbam = await m.reply("Gbaning...")
       await gbam.edit_text(gbam_text.format(chat.username,user1.mention,
                                             user2.mention,reason,count))
        
