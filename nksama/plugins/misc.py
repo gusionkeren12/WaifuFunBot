2a008a221860aae1bd1c219e5e5e1c9e16a6a947
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-28 08:33:42 +0530

    Update misc.py

--- nksama/plugins/misc.py
+++ nksama/plugins/misc.py
@@ -1,7 +1,7 @@
 import os
 from pyrogram import filters
 from pyrogram.types import Message
-
+from gpytranslate import Translator
 from nksama import bot as app, bot, arq
 from nksama.utils.errors import capture_err
 from nksama.utils.http import get
@@ -9,28 +9,37 @@ from nksama.utils.http import get
 
 
 
-# Translate
-@bot.on_message(filters.command('tr'))
-@capture_err
-async def tr(_, message):
-    if len(message.command) != 2:
-        return await message.reply_text("/tr [LANGUAGE_CODE]")
-    lang = message.text.split(None, 1)[1]
-    if not message.reply_to_message or not lang:
-        return await message.reply_text(
-            "Reply to a message with /tr [language code]"
-            + "\nGet supported language list from here -"
-            + " https://py-googletrans.readthedocs.io/en"
-            + "/latest/#googletrans-languages"
-        )
-    reply = message.reply_to_message
-    text = reply.text or reply.caption
-    if not text:
-        return await message.reply_text("Reply to a text to translate it")
-    result = await arq.translate(text, lang)
-    if not result.ok:
-        return await message.reply_text(result.result)
-    await message.reply_text(result.result.translatedText)
+trans = Translator()
+
+
+@bot.on_message(filters.command(["tl", "tr"]))
+async def translate(_, message: Message) -> None:
+    reply_msg = message.reply_to_message
+    if not reply_msg:
+        await message.reply_text("Reply to a message to translate it!")
+        return
+    if reply_msg.caption:
+        to_translate = reply_msg.caption
+    elif reply_msg.text:
+        to_translate = reply_msg.text
+    try:
+        args = message.text.split()[1].lower()
+        if "//" in args:
+            source = args.split("//")[0]
+            dest = args.split("//")[1]
+        else:
+            source = await trans.detect(to_translate)
+            dest = args
+    except IndexError:
+        source = await trans.detect(to_translate)
+        dest = "en"
+    translation = await trans(to_translate, sourcelang=source, targetlang=dest)
+    reply = (
+        f"<b>Translated from {source} to {dest}</b>:\n"
+        f"<code>{translation.text}</code>"
+    )
+
+    await message.reply_text(reply, parse_mode="html")
 
 
 
