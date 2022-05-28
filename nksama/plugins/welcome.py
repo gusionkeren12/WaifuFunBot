33a3c5e23bc396ed3ec0667c8b59a528b803b05e
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-28 16:07:35 +0530

    Update welcome.py

--- nksama/plugins/welcome.py
+++ nksama/plugins/welcome.py
@@ -6,8 +6,10 @@ WEL_GIF = "https://telegra.ph/file/b616d3a126715d9b1aa46.mp4"
 
 @bot.on_message(filters.new_chat_members)
 async def welcome(_, m: Message):
+        new = await m.reply_text("Scanning...")
+        new.delete()
         await m.reply_animation(WEL_GIF,caption="hello! dear {}\nWelcome to **{}**".format(m.from_user.mention,m.chat.title))
         
 @bot.on_message(filters.left_chat_member)
-async def left(_, m: Message):
+async def member_has_left(_, m: Message):
         await m.reply("oof! dear {}\nlefted to **{}**".format(m.from_user.mention,m.chat.title))
