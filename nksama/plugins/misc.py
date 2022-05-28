a5082611b07786770f5f55b9ed7fed2231e6ae0e
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-28 09:34:27 +0530

    Update misc.py

--- nksama/plugins/misc.py
+++ nksama/plugins/misc.py
@@ -8,7 +8,7 @@ from nksama import bot
 
 
 @bot.on_message(filters.command(["lang", "langs"]))
-def language(_, m: Message)
+def language(_, m: Message):
        #langs codes
        await m.reply_text("Click on the button below to see the list of supported language codes.",
         reply_markup=InlineKeyboardMarkup(
