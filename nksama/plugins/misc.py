232596ae12ffc68fee5dde0b1a74516843ae7bff
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-29 08:50:28 +0530

    Update misc.py

--- nksama/plugins/misc.py
+++ nksama/plugins/misc.py
@@ -91,8 +91,7 @@ async def ud(_, message: Message):
         f'https://api.urbandictionary.com/v0/define?term={text}').json()
     reply_text = f'**results: {text}**\n\n{results["list"][0]["definition"]}\n\n_{results["list"][0]["example"]}_'
     await message.reply_text(reply_text)
-       
-     else:
-         message.reply("Not Found!")
+       else:
+           message.reply("Not Found!")
 
 
