dc51019f6b8482e96a78136a27661fb9b4713a46
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-30 06:49:27 +0530

    Update fun.py

--- nksama/plugins/fun.py
+++ nksama/plugins/fun.py
@@ -2,42 +2,7 @@ from pyrogram.types import Message
 from pyrogram import filters
 from nksama import bot, BOT_ID
 import random
-
-dnote = [ ]
-
-deathnote_img = "https://telegra.ph/file/9e220eb59606f8435eafe.mp4"
-
-@bot.on_message(filters.command('adddeathnote'))
-def adddeathnote(_, m: Message):
-    reply = m.reply_to_message
-    if reply:
-        dnote.append(reply.from_user.first_name)
-        reply.reply_animation(deathnote_img,
-                              caption="{} Added Death Note List!".format(reply.from_user.mention))
-        
-    else:
-        m.reply_animation(deathnote_img,
-                              caption="{} Added Death Note List!".format(m.from_user.mention))
-  
-@bot.on_message(filters.command('removedeathnote'))
-def removedeathnote(_, m: Message):
-    reply = m.reply_to_message
-    if reply:
-        dnote.remove(reply.from_user.first_name)
-        reply.reply_animation(deathnote_img,
-                              caption="{} Removed Death Note List!".format(reply.from_user.mention))
-        
-    else:
-        m.reply("reply to Death Note list added person!")
-  
-
-@bot.on_message(filters.command('deathnotelist'))
-def deathnotelist(_, m):
-    reply = m.reply_to_message
-    if reply:
-        reply(str(dnote))
-    else:
-        m.reply(str(dnote))
+import requests 
         
         
 @bot.on_message(filters.regex('good morning'))
@@ -86,3 +51,16 @@ async def gbams(_, m: Message):
       await gbam.edit_caption(gbam_text.format(chat.username,user1.mention,
                                             user2.mention,reason,count))
        
+@bot.on_message(filters.command('meme'))
+def meme(_,message: Message):
+        res = requests.get('https://some-random-api.ml/meme').json()
+        url = res['image']
+        text = res['caption']
+        message.reply_photo(photo=url, caption=text)
+
+      
+@bot.on_message(filters.command("joke"))
+def joke(_, message: Message):
+        res = requests.get('https://some-random-api.ml/joke').json()
+        text = res['joke']
+        message.reply_text(text)
