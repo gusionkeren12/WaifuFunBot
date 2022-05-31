204f19402a0ce739cf6f0301037adc053137a5cc
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-31 19:00:42 +0530

    Update start_help_text.py

--- nksama/plugins/start_help_text.py
+++ nksama/plugins/start_help_text.py
@@ -61,7 +61,7 @@ here the anime help & commads
 """
 
 @bot.on_callback_query(filters.regex("_help"))
-async def help_back(query: CallbackQuery, message):
-    await query.message.edit_caption("test man")
+async def help_back(query: CallbackQuery):
+    await query.edit_caption("test man")
                
         
