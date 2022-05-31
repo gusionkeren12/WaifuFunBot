82774b2f0404be19bbe2f494b46d41baa868c43d
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-31 22:20:51 +0530

    Update start_help_text.py

--- nksama/plugins/start_help_text.py
+++ nksama/plugins/start_help_text.py
@@ -77,7 +77,6 @@ anime themed fun & search:
 
 @bot.on_callback_query(filters.regex("anime_help"))
 async def ahelp(_, query: CallbackQuery):
-     await query.message.edit_caption(ANIME_TEXT),
-     reply_markup=InlineKeyboardMarkup(
+     await query.message.edit_caption(ANIME_TEXT),reply_markup=InlineKeyboardMarkup(
         [[InlineKeyboardButton('back 🔙', callback_data='help_back'),
           InlineKeyboardButton('close 🗑', callback_data='close')]],)
