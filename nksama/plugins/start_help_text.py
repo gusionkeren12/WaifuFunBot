f87ae727549df0255ad1c6f166a7b34a2f851a79
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-01 07:28:01 +0530

    Update start_help_text.py

--- nksama/plugins/start_help_text.py
+++ nksama/plugins/start_help_text.py
@@ -56,17 +56,14 @@ async def start(_, m: Message):
   
 @bot.on_callback_query(filters.regex("help_back"))
 async def help(_, query: CallbackQuery):
-    mention = query.message.reply_to_message.from_user.mention or query.message.from_user.mention
-    await query.message.edit_caption(HELP_TEXT.format(mention),
+    await query.message.edit_caption(HELP_TEXT,
                                     reply_markup=InlineKeyboardMarkup(HELP_BUTTON),)
                
 @bot.on_callback_query(filters.regex("close"))
 async def close(_, query: CallbackQuery):
            query = query.message
-           elif (query.from_user.mention or query.reply_to_message.from_user.mention):
-           mention = query.reply_to_message.from_user.mention or query.from_user.mention
            await query.delete()
-           await bot.send_message(query.chat.id, 
+           await bot.send_messagwe(query.chat.id, 
                                   f"query message deleted by {mention}")
  
 ANIME_TEXT = """
